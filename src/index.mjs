f3dbf548ba333f1eabe58adac5c78595df230d26
Duude92 <switch4acc@gmail.com>
2025-04-30 13:03:34 +0300

    Usage of pathHandler

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,13 @@
 import process from 'node:process'
 import * as messageManager from './messages/messageManager.mjs';
-import fs from 'node:fs';
 import { Transform } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
+import { createPathHandler } from './pathHandler.js';
 // I've added verification if the username is provided in the command line arguments, if not it will use 'Anonymous' as default.
 const username = process.argv[2] && process.argv[2].split('=')[1] || 'Anonymous';
 messageManager.displayGreeting(username);
+const pathHandler = createPathHandler();
+messageManager.displayCwd(pathHandler.cwd);
 
 let transformStream = new Transform({
     transform(chunk, encoding, callback) {
