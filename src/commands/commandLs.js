02da137c4349b910f2d40b1633ec3fe45888b8ae
Duude92 <switch4acc@gmail.com>
2025-04-30 15:02:11 +0300

    Fix readdir parameter

--- src/commands/commandLs.js
+++ src/commands/commandLs.js
@@ -1,15 +1,14 @@
 import fs from 'node:fs/promises';
 import { CommandBase } from './commandBase.js';
-class CommandLs extends CommandBase{
-    constructor(){
+class CommandLs extends CommandBase {
+    constructor() {
         super('ls');
     }
-    async performCommand(cwd, args){
-        try{
-            if(!!args) return await fs.readdir(args[0]);
-            return await fs.readdir('./');
+    async performCommand(cwd, args) {
+        try {
+            return await fs.readdir((!!args[0]) && args[0] || cwd);
         }
-        catch(err){
+        catch (err) {
             console.error('Error reading directory:', err);
             return 'Error reading directory';
         }
