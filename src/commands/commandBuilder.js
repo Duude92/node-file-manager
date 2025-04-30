16ca6f79c35c76408e5703c2d77b30655228f78e
Duude92 <switch4acc@gmail.com>
2025-04-30 14:48:51 +0300

    Added CommandBuilder class, which would select command from command string

---
+++ src/commands/commandBuilder.js
@@ -0,0 +1,14 @@
+import { createCommandLs } from "./commandLs.js";
+
+class CommandBuilder{
+    constructor() {
+        this._commandList = [createCommandLs()];
+    }
+
+    getCommand = (command) => {
+        const commandString = command.split(' ')[0];
+        let commandObject = this._commandList.find((commandObj) => commandObj.supportsCommand(commandString));
+        return commandObject;
+    }
+}
+export const createCommandBuilder = () => new CommandBuilder()
