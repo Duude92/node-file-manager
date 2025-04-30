eed59445da0b4a6b2d60a516699262ae993adaf7
Duude92 <switch4acc@gmail.com>
2025-04-30 14:49:54 +0300

    Changed usage of command TransformStream

--- src/index.mjs
+++ src/index.mjs
@@ -3,14 +3,13 @@ import * as messageManager from './messages/messageManager.mjs';
 import { Transform } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
 import { createPathHandler } from './pathHandler.js';
-import { transformStream as commandTransform } from './commands/commandTransform.js';
+import { createTransformStream } from './commands/commandTransform.js';
 // I've added verification if the username is provided in the command line arguments, if not it will use 'Anonymous' as default.
 const username = process.argv[2] && process.argv[2].split('=')[1] || 'Anonymous';
 messageManager.displayGreeting(username);
 const pathHandler = createPathHandler();
 messageManager.displayCwd(pathHandler.cwd);
 
-
-await pipeline(process.stdin, commandTransform, process.stdout, { end: false });
+await pipeline(process.stdin, createTransformStream(), process.stdout, { end: false });
 
 messageManager.displayGoodbye(username);
