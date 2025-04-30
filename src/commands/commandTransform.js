f6dedd977b9cc3c0bf773aebea7c46fddcfbdf04
Duude92 <switch4acc@gmail.com>
2025-04-30 17:24:01 +0300

    Separate result into function, called function to display cwd

--- src/commands/commandTransform.js
+++ src/commands/commandTransform.js
@@ -2,29 +2,33 @@ import { Transform } from 'node:stream';
 import { createCommandBuilder } from './commandBuilder.js';
 import os from 'node:os';
 import { getPathHandler } from '../pathHandler.js';
+import { displayCwd } from '../messages/messageManager.mjs';
 const commandBuilder = createCommandBuilder();
 const pathHandler = getPathHandler();
 const transformStream = new Transform({
     transform(chunk, encoding, callback) {
+        const input = chunk.toString().trim();
+        const displayResult = (result) => {
+            displayCwd();
+
+            this.push(`${result}${os.EOL}> `);
+            callback();
+        }
         try {
-            const command = commandBuilder.getCommand(chunk.toString().trim());
-            const args = chunk.toString().trim().split(' ').slice(1);
+            const command = commandBuilder.getCommand(input);
+            const args = input.split(' ').slice(1);
             command.performCommand(pathHandler.cwd, args).then((result) => {
                 try {
-                    this.push(os.EOL + '> ');
-                    callback();
+                    displayResult('');
                 }
                 catch (error) {
-                    this.push(`Operation failed:\n ${error}` + os.EOL + '> ');
-                    callback();
+                    displayResult(`Operation failed:\n ${error}`);
                 }
             });
         }
         catch (error) {
-            this.push(`Invalid input: ${chunk.toString().trim()}` + os.EOL + '> ');
-            callback();
+            displayResult(`Invalid input: ${input}`);
         }
-
     }
 });
 export const createTransformStream = () => transformStream;
