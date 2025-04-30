00bc53b2c9ca479d3d472fc1906d729684abf40f
Duude92 <switch4acc@gmail.com>
2025-04-30 12:47:08 +0300

    Added messages for cwd

--- src/messages/messageManager.mjs
+++ src/messages/messageManager.mjs
@@ -1,3 +1,4 @@
 import * as messages from './messages.mjs'
 export const displayGreeting = (username) => console.log(messages.greeting(username));
 export const displayGoodbye = (username) => console.log(messages.goodbye(username));
+export const displayCwd = (directory) => console.log(messages.cwd(directory));
