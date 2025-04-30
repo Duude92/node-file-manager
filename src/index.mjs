16e0bef44bd100fcee8175f72f81cc957e0c6f3e
Duude92 <switch4acc@gmail.com>
2025-04-30 16:29:43 +0300

    Implement interrupt signal for terminal to exit from app

--- src/index.mjs
+++ src/index.mjs
@@ -3,11 +3,16 @@ import * as messageManager from './messages/messageManager.mjs';
 import { pipeline } from 'node:stream/promises';
 import { createTransformStream } from './commands/commandTransform.js';
 import { getPathHandler } from './pathHandler.js';
+import os from 'node:os';
 // I've added verification if the username is provided in the command line arguments, if not it will use 'Anonymous' as default.
 const username = process.argv[2] && process.argv[2].split('=')[1] || 'Anonymous';
 const pathHandler = getPathHandler();
 messageManager.displayGreeting(username);
 messageManager.displayCwd(pathHandler.cwd);
 process.stdout.write('> ');
+process.on('SIGINT', () => {
+    process.stdout.write(os.EOL);
+    messageManager.displayGoodbye(username);
+    process.exit(0);
+});
 await pipeline(process.stdin, createTransformStream(), process.stdout, { end: false });
-messageManager.displayGoodbye(username);
