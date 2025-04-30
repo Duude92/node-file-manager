438b9f065cbb7d32ed87d454182ef83d7a0a9ce4
Duude92 <switch4acc@gmail.com>
2025-04-30 15:02:39 +0300

    Provide cwd for each command by default

--- src/commands/commandTransform.js
+++ src/commands/commandTransform.js
@@ -1,12 +1,14 @@
 import { Transform } from 'node:stream';
 import { createCommandBuilder } from './commandBuilder.js';
 import os from 'node:os';
+import { getPathHandler } from '../pathHandler.js';
 const commandBuilder = createCommandBuilder();
+const pathHandler = getPathHandler();
 const transformStream = new Transform({
     transform(chunk, encoding, callback) {
         const command = commandBuilder.getCommand(chunk.toString().trim());
         const args = chunk.toString().trim().split(' ').slice(1);
-        command.performCommand(args).then((result) => {
+        command.performCommand(pathHandler.cwd, args).then((result) => {
             result = result.join('\n') + os.EOL;
             this.push(result);
             callback();
