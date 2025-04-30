2c08b02e0bb04f01e5b18ad484c2deac23c739fb
Duude92 <switch4acc@gmail.com>
2025-04-30 12:25:47 +0300

    Added username anonymous verification

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
 import process from 'node:process'
 import * as messageManager from './messages/messageManager.mjs';
 import fs from 'node:fs';
-const username = process.argv[2]&&process.argv[2].split('=')[1];
+// I've added verification if the username is provided in the command line arguments, if not it will use 'Anonymous' as default.
+const username = process.argv[2] && process.argv[2].split('=')[1] || 'Anonymous';
 messageManager.displayGreeting(username);
