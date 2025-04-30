f26b2316da16df772f154aa0de681fddbb13f9ba
Duude92 <switch4acc@gmail.com>
2025-04-30 17:31:03 +0300

    Added command Up, to change cwd to parent directory

---
+++ src/commands/commandUp.js
@@ -0,0 +1,13 @@
+import { getPathHandler } from "../pathHandler.js";
+import { CommandBase } from "./commandBase.js";
+
+class CommandUp extends CommandBase{
+    constructor() {
+        super('up');
+        this._pathHandler = getPathHandler();
+    }
+    async performCommand(cwd, args) {
+        this._pathHandler.cd('..');
+    }
+}
+export const createCommandUp = () => new CommandUp();
