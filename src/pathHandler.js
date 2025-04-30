ec11c158ff9d79bbedf42f16cd02960b0803dc0b
Duude92 <switch4acc@gmail.com>
2025-04-30 21:06:26 +0300

    Added command ls to retrive Dirent array from path, added resolvePath to get absolute path from relative

--- src/pathHandler.js
+++ src/pathHandler.js
@@ -1,5 +1,6 @@
 import os from 'node:os'
 import path from 'node:path';
+import fs from 'node:fs/promises';
 class PathHandler {
     constructor(homePath) {
         this._homepath = homePath;
@@ -11,8 +12,15 @@ class PathHandler {
     get homepath() {
         return this._homepath;
     }
-    cd(relativePath){
+    cd(relativePath) {
         this._cwd = path.resolve(this._cwd, relativePath);
+    }
+    async ls(relativePath) {
+        const nPath = path.resolve(this._cwd, relativePath);
+        return await fs.readdir(nPath, { withFileTypes: true })
+    }
+    resolvePath(relativePath) {
+        return path.resolve(this._cwd, relativePath);
     }
 
 }
