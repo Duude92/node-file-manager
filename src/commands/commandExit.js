e6cfe5e57bfa6c4f8776d0d591fba2770618245a
Duude92 <switch4acc@gmail.com>
2025-04-30 17:01:59 +0300

    Implementation of exit command

---
+++ src/commands/commandExit.js
@@ -0,0 +1,19 @@
+import { CommandBase } from "./commandBase.js";
+import * as messageManager from '../messages/messageManager.mjs';
+// Include both 'exit' and '.exit' as valid commands
+const regexp = /^\.?exit/;
+
+class CommandExit extends CommandBase {
+    constructor() {
+        super('exit');
+        this._regexp = new RegExp(regexp);
+    }
+    supportsCommand(commandstring){
+        return this._regexp.test(commandstring);
+    }
+    async performCommand(cwd, args) {
+        messageManager.displayGoodbye();
+        process.exit(0);
+    }
+}
+export const createCommandExit = () => new CommandExit();
