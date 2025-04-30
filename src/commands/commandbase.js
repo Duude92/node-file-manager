98acd4ed9e16d05e79de68ab84f5d7d8bc5f1d73
Duude92 <switch4acc@gmail.com>
2025-04-30 21:02:54 +0300

    Compose CommandBase with PathHandler

--- src/commands/commandbase.js
+++ src/commands/commandbase.js
@@ -1,6 +1,9 @@
+import { getPathHandler } from "../pathHandler.js";
+
 export class CommandBase {
     constructor(commandstring){
         this._command = commandstring;
+        this._pathHandler = getPathHandler();
     }
     supportsCommand(commandstring){
         return this._command === commandstring;
