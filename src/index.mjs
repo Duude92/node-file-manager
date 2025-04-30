825fd5670f2bd3558eb0b14a93d1d51b6bea7ee9
Duude92 <switch4acc@gmail.com>
2025-04-30 16:04:21 +0300

    Added welcome command line message '> '

--- src/index.mjs
+++ src/index.mjs
@@ -5,10 +5,9 @@ import { createTransformStream } from './commands/commandTransform.js';
 import { getPathHandler } from './pathHandler.js';
 // I've added verification if the username is provided in the command line arguments, if not it will use 'Anonymous' as default.
 const username = process.argv[2] && process.argv[2].split('=')[1] || 'Anonymous';
-messageManager.displayGreeting(username);
 const pathHandler = getPathHandler();
+messageManager.displayGreeting(username);
 messageManager.displayCwd(pathHandler.cwd);
-
+process.stdout.write('> ');
 await pipeline(process.stdin, createTransformStream(), process.stdout, { end: false });
-
 messageManager.displayGoodbye(username);
