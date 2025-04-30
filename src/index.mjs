20b05751f719eb58c6ea9a03a73e66e7e0564a91
Duude92 <switch4acc@gmail.com>
2025-04-30 12:40:14 +0300

    Basic input configuration

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,19 @@
 import process from 'node:process'
 import * as messageManager from './messages/messageManager.mjs';
 import fs from 'node:fs';
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
 // I've added verification if the username is provided in the command line arguments, if not it will use 'Anonymous' as default.
 const username = process.argv[2] && process.argv[2].split('=')[1] || 'Anonymous';
 messageManager.displayGreeting(username);
 
-
+let transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+        const data = chunk.toString().toUpperCase();
+        this.push(data);
+        callback();
+    }
+});
+await pipeline(process.stdin, transformStream, process.stdout, { end: false });
 
 messageManager.displayGoodbye(username);
