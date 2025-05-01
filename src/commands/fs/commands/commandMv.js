8eacb784fef248725db8b123b5785faf58bcfe23
Duude92 <switch4acc@gmail.com>
2025-05-01 22:07:46 +0300

    Fix mv command, in accordance to specification // same as copy but initial file is deleted, copying part should be done using Readable and Writable streams

--- src/commands/fs/commands/commandMv.js
+++ src/commands/fs/commands/commandMv.js
@@ -1,5 +1,8 @@
 import { CommandBase } from "../../commandBase.js";
-import fs from 'node:fs/promises';
+import fsPromise from 'node:fs/promises';
+import fs from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { displayResultLine } from "../../../messages/messageManager.mjs";
 
 class CommandMv extends CommandBase {
     constructor() {
@@ -12,7 +15,12 @@ class CommandMv extends CommandBase {
         }
         const sourcePath = this._pathHandler.resolvePath(args[0]);
         const destinationPath = this._pathHandler.resolvePath(args[1]);
-        await fs.rename(sourcePath, destinationPath);
+        await fsPromise.access(sourcePath, fsPromise.constants.R_OK);
+        const sourceStream = fs.createReadStream(sourcePath);
+        const destinationStream = fs.createWriteStream(destinationPath, { flags: 'wx' });
+        await pipeline(sourceStream, destinationStream);
+        await fsPromise.unlink(this._pathHandler.resolvePath(sourcePath));
+        displayResultLine(`${sourcePath} successfully moved to ${destinationPath}`);
     }
 }
 export const createCommand = () => new CommandMv();
