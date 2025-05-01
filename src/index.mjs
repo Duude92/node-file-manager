69ccc9dd626380241de2f1877b9bc50d10da7081
Duude92 <switch4acc@gmail.com>
2025-05-01 14:22:59 +0300

    Fix powershell nom processing args

--- src/index.mjs
+++ src/index.mjs
@@ -2,12 +2,19 @@ import process from 'node:process'
 import * as messageManager from './messages/messageManager.mjs';
 import { pipeline } from 'node:stream/promises';
 import { createTransformStream } from './commands/commandTransform.js';
-import { getPathHandler } from './pathHandler.js';
 import os from 'node:os';
+const DOUBLE_HYPHEN_ONLY = true;
 // I've added verification if the username is provided in the command line arguments, if not it will use 'Anonymous' as default.
-const username = process.argv[2] && process.argv[2].split('=')[1] || 'Anonymous';
-const pathHandler = getPathHandler();
-messageManager.setUsername(username);
+const getUsername = () => {
+    // Fix powershell npm processing arguments with double hyphen
+    // See the https://discord.com/channels/755676888680366081/1366674843852079124/1367447759812300831
+    if (!!process.env.npm_config_username) return process.env.npm_config_username;
+    const arg = process.argv.find(arg => arg.includes(`${DOUBLE_HYPHEN_ONLY ? '--' : ''}username`));
+    return arg && arg.split('=')[1]
+        || 'Anonymous';
+}
+
+messageManager.setUsername(getUsername());
 messageManager.displayGreeting();
 messageManager.displayCwd();
 process.stdout.write('> ');
@@ -16,4 +23,4 @@ process.on('SIGINT', () => {
     messageManager.displayGoodbye();
     process.exit(0);
 });
-await pipeline(process.stdin, createTransformStream(), process.stdout, { end: false });
+await pipeline(process.stdin, createTransformStream(), process.stdout, { end: false }); 
