9df8bf924ac8c366ca87dcec985f342b072fe7d0
Duude92 <switch4acc@gmail.com>
2025-05-01 18:19:44 +0300

    Move anonymous username to appconfig

--- src/appconfig.js
+++ src/appconfig.js
@@ -1,2 +1,3 @@
 export const DOUBLE_HYPHEN_ONLY = false;
 export const STRICT_EXIT = true;
+export const DEFAULT_USERNAME = 'Anonymous';
--- src/index.mjs
+++ src/index.mjs
@@ -4,7 +4,7 @@ import { pipeline } from 'node:stream/promises';
 import { createTransformStream } from './commands/commandTransform.js';
 import os from 'node:os';
 import { getUserHandler } from './userHandler.js';
-import { DOUBLE_HYPHEN_ONLY } from './appconfig.js';
+import { DEFAULT_USERNAME, DOUBLE_HYPHEN_ONLY } from './appconfig.js';
 // I've added verification if the username is provided in the command line arguments, if not it will use 'Anonymous' as default.
 const getUsername = () => {
     // Fix powershell npm processing arguments with double hyphen
@@ -12,7 +12,7 @@ const getUsername = () => {
     if (!!process.env.npm_config_username) return process.env.npm_config_username;
     const arg = process.argv.find(arg => arg.includes(`${DOUBLE_HYPHEN_ONLY ? '--' : ''}username`));
     return arg && arg.split('=')[1]
-        || 'Anonymous';
+        || DEFAULT_USERNAME;
 }
 let userhandler = getUserHandler();
 userhandler.username = getUsername();
