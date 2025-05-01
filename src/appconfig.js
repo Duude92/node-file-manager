e3bda9561d0d69b34d40cc5e4ee56917125ac056
Duude92 <switch4acc@gmail.com>
2025-05-01 18:11:58 +0300

    Added STRICT_EXIT constant to appconfig to make it possible to use  '.exit' only or ('exit' or '.exit') command

--- src/appconfig.js
+++ src/appconfig.js
@@ -1,1 +1,2 @@
 export const DOUBLE_HYPHEN_ONLY = false;
+export const STRICT_EXIT = true;
--- src/commands/commandExit.js
+++ src/commands/commandExit.js
@@ -1,15 +1,15 @@
 import { CommandBase } from "./commandBase.js";
 import * as messageManager from '../messages/messageManager.mjs';
 import { getUserHandler } from "../userHandler.js";
+import { STRICT_EXIT } from "../appconfig.js";
 // Include both 'exit' and '.exit' as valid commands
-const regexp = /^\.?exit$/;
-
+const regexp = STRICT_EXIT ? /^\.exit$/ : /^\.?exit$/;
 class CommandExit extends CommandBase {
     constructor() {
         super('exit');
         this._regexp = new RegExp(regexp);
     }
-    supportsCommand(commandstring){
+    supportsCommand(commandstring) {
         return this._regexp.test(commandstring);
     }
     async performCommand(args) {
