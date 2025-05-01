a9591f89bf05042b38381a49a70249668266f2d4
Duude92 <switch4acc@gmail.com>
2025-05-01 16:07:42 +0300

    Fix command resolving

--- src/commands/os/os.js
+++ src/commands/os/os.js
@@ -2,10 +2,11 @@ import { displayError } from "../../messages/messageManager.mjs";
 import { CommandBase } from "../commandBase.js";
 import { getCommands } from "../commandLoader.js";
 
+const parameters = await getCommands('./os/');
 class CommandOs extends CommandBase {
     constructor() {
         super('os');
-        this._parameters = getCommands('./os/');
+        this._parameters = parameters;
     }
     async performCommand(args) {
         if (args.length === 0) {
@@ -19,10 +20,10 @@ Options:
     --architecture      Display the architecture of the operating system.`);
             return;
         }
-        const command = this.getCommand(args[0]);
+        const command = await this.getCommand(args[0]);
         command.performCommand();
     }
-    getCommand = (command) => {
+    getCommand = async (command) => {
         const commandString = command.split(' ')[0];
         let commandObject = this._parameters.find((commandObj) => commandObj.supportsCommand(commandString));
         return commandObject;
