5808c687fe0e0499e1b4fe3b5a8de462bd4b0867
Duude92 <switch4acc@gmail.com>
2025-05-01 11:05:48 +0300

    Added hash command

---
+++ src/commands/utilities/utilitycommands/commandHash.js
@@ -0,0 +1,22 @@
+import { CommandBase } from "../../commandBase.js";
+import fs from "node:fs/promises";
+import crypto from "node:crypto";
+import { displayResultLine } from "../../../messages/messageManager.mjs";
+
+class CommandHash extends CommandBase {
+    constructor() {
+        super("hash");
+    }
+
+    async performCommand(cwd, args) {
+        if (args.length === 0) {
+            throw new Error("No arguments provided for hash command.");
+        }
+        const filePath = this._pathHandler.resolvePath(args[0]);
+
+        const fileContent = await fs.readFile(filePath, { encoding: "utf-8" });
+        const hash = crypto.createHash("sha256").update(fileContent).digest("hex");
+        displayResultLine(`Hash of ${filePath}: ${hash}`);
+    }
+}
+export const createCommandHash = () => new CommandHash();
