ab74dd615fefc25c12346ba571146273a76f3f80
Duude92 <switch4acc@gmail.com>
2025-05-01 22:06:55 +0300

    Fix cat command // added eol

--- src/commands/fs/commands/commandCat.js
+++ src/commands/fs/commands/commandCat.js
@@ -1,6 +1,7 @@
 import { CommandBase } from "../../commandBase.js";
 import { createReadStream } from "node:fs";
 import { pipeline } from "node:stream/promises";
+import { EOL } from "node:os";
 
 const encoding = 'utf8';
 
@@ -11,6 +12,7 @@ class CommandCat extends CommandBase {
     async performCommand(args) {
         const filePath = this._pathHandler.resolvePath(args[0]);
         await pipeline(createReadStream(filePath, encoding), process.stdout, { end: false });
+        process.stdout.write(EOL);
     }
 }
 export const createCommand = () => new CommandCat();
