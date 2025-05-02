731e3f7632dd4c0a5e0bd152b78aff6388d1360f
Duude92 <switch4acc@gmail.com>
2025-05-02 21:26:52 +0300

    Implementation of parameters validation, usage and description for command rn

--- src/commands/fs/commands/commandRn.js
+++ src/commands/fs/commands/commandRn.js
@@ -5,13 +5,16 @@ import {displayResultLine} from "../../../messages/messageManager.mjs";
 class CommandRn extends CommandBase {
     constructor() {
         super('rn');
+        this._usage = `rn [FILENAME] [NEWNAME]`;
+        this._description = `Renames FILENAME to NEWNAME`;
+    }
+
+    validateParameters(args) {
+        return args.length > 1;
     }
 
     // TODO: test if newpath already exist
     async performCommand(args) {
-        if (args.length < 2) {
-            throw new Error('Not enough arguments. Usage: rn <oldname> <newname>');
-        }
         const oldPath = this._pathHandler.resolvePath(args[0]);
         const newPath = this._pathHandler.resolvePath(args[1]);
         try {
