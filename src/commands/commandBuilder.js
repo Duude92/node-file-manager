88c1c73dc55b0e7a43b912702ea9536c42c76ccf
Duude92 <switch4acc@gmail.com>
2025-05-02 15:01:03 +0300

    Added some JSDoc annotation

--- src/commands/commandBuilder.js
+++ src/commands/commandBuilder.js
@@ -1,10 +1,14 @@
-import { fsCommands } from './fs/fs.js'
-import { createCommandExit } from "./commandExit.js";
-import { utilities } from './utilities/utilities.js';
-import { createCommandOs } from './os/os.js';
+import {fsCommands} from './fs/fs.js'
+import {createCommandExit} from "./commandExit.js";
+import {utilities} from './utilities/utilities.js';
+import {createCommandOs} from './os/os.js';
 
 class CommandBuilder {
     constructor() {
+        /**
+         * @type {CommandBase[]}
+         * @private
+         */
         this._commandList = [
             createCommandExit(),
             ...fsCommands,
@@ -13,10 +17,13 @@ class CommandBuilder {
         ];
     }
 
+    /**
+     * @param command string command
+     * @returns {CommandBase} object
+     */
     getCommand = (command) => {
         const commandString = command.split(' ')[0];
-        let commandObject = this._commandList.find((commandObj) => commandObj.supportsCommand(commandString));
-        return commandObject;
+        return this._commandList.find((commandObj) => commandObj.supportsCommand(commandString));
     }
 }
 export const createCommandBuilder = () => new CommandBuilder()
