5f85b753c2df761ce0b1da235c3422336fd8e89a
Duude92 <switch4acc@gmail.com>
2025-05-02 11:41:42 +0300

    Fix commandRn to display message on successfully completed operation

--- src/commands/fs/commands/commandRn.js
+++ src/commands/fs/commands/commandRn.js
@@ -1,10 +1,12 @@
-import { CommandBase } from "../../commandBase.js";
+import {CommandBase} from "../../commandBase.js";
 import fs from 'node:fs/promises';
+import {displayResultLine} from "../../../messages/messageManager.mjs";
 
 class CommandRn extends CommandBase {
     constructor() {
         super('rn');
     }
+
     // TODO: test if newpath already exist
     async performCommand(args) {
         if (args.length < 2) {
@@ -13,6 +15,8 @@ class CommandRn extends CommandBase {
         const oldPath = this._pathHandler.resolvePath(args[0]);
         const newPath = this._pathHandler.resolvePath(args[1]);
         await fs.rename(oldPath, newPath);
+        displayResultLine(`File ${args[0]} was renamed to ${args[1]}`);
     }
 }
+
 export const createCommand = () => new CommandRn();
