303a38fb5d7979bd9082e2d1c1df2f9a8b70f828
Duude92 <switch4acc@gmail.com>
2025-05-02 15:58:54 +0300

    Implementation of parameters validation, usage and description for command cd

--- src/commands/fs/commands/commandCd.js
+++ src/commands/fs/commands/commandCd.js
@@ -1,11 +1,17 @@
-import { getPathHandler } from "../../../pathService.js";
-import { CommandBase } from "../../commandBase.js";
+import {getPathHandler} from "../../../pathService.js";
+import {CommandBase} from "../../commandBase.js";
+import {EOL} from "node:os";
 
 class CommandCd extends CommandBase {
     constructor() {
         super('cd');
         this._pathHandler = getPathHandler();
+        this._usage = `cd [PATH?]`;
+        this._description = `Changes CWD to PATH.${EOL}If [PATH] is not provided, CWD would be changed to HOMEDIR`;
     }
+
+    validateParameters = (args) => true;
+
     async performCommand(args) {
         if (args.length === 0) {
             await this._pathHandler.cd(this._pathHandler.homePath);
@@ -15,4 +21,5 @@ class CommandCd extends CommandBase {
         await this._pathHandler.cd(path);
     }
 }
+
 export const createCommand = () => new CommandCd();
