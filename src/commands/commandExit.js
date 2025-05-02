efa4ddceebf42750e5325484495ec5e7e4ef0b9c
Duude92 <switch4acc@gmail.com>
2025-05-02 22:21:25 +0300

    Usage of #CommandBase // code cleaning

--- src/commands/commandExit.js
+++ src/commands/commandExit.js
@@ -1,20 +1,23 @@
-import { CommandBase } from "./commandBase.js";
-import * as messageManager from '../messages/messageManager.mjs';
-import { getUserHandler } from "../userHandler.js";
-import { STRICT_EXIT } from "../appconfig.js";
+import {CommandBase} from "#CommandBase";
+import {getUserHandler} from "../userHandler.js";
+import {STRICT_EXIT} from "../appconfig.js";
 // Include both 'exit' and '.exit' as valid commands
 const regexp = STRICT_EXIT ? /^\.exit$/ : /^\.?exit$/;
+
 class CommandExit extends CommandBase {
     constructor() {
         super('exit');
         this._regexp = new RegExp(regexp);
     }
-    supportsCommand(commandstring) {
-        return this._regexp.test(commandstring);
+
+    supportsCommand(commandString) {
+        return this._regexp.test(commandString);
     }
+
     async performCommand(args) {
         getUserHandler().goodbye();
         process.exit(0);
     }
 }
+
 export const createCommandExit = () => new CommandExit();
