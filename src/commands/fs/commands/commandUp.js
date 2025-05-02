fc97cee103ec337329063cc0dc35af6657c2d64a
Duude92 <switch4acc@gmail.com>
2025-05-02 21:27:54 +0300

    Implementation of parameters validation, usage and description for command up

--- src/commands/fs/commands/commandUp.js
+++ src/commands/fs/commands/commandUp.js
@@ -1,13 +1,19 @@
-import { getPathHandler } from "../../../pathService.js";
-import { CommandBase } from "../../commandBase.js";
+import {getPathHandler} from "../../../pathService.js";
+import {CommandBase} from "../../commandBase.js";
 
 class CommandUp extends CommandBase {
     constructor() {
         super('up');
         this._pathHandler = getPathHandler();
+        this._usage = `up`;
+        this._description = `Go upper from current directory.`;
     }
+
+    validateParameters = (args) => true;
+
     async performCommand(args) {
         await this._pathHandler.cd('..');
     }
 }
+
 export const createCommand = () => new CommandUp();
