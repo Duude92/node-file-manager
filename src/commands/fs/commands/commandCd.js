ce7465b3a50b0a28c7a02f7992fef10388989d97
Duude92 <switch4acc@gmail.com>
2025-05-02 16:13:54 +0300

    Implementation of parameters validation, usage and description for command cd // changed cd behaviour (no directory change if no path provided)

--- src/commands/fs/commands/commandCd.js
+++ src/commands/fs/commands/commandCd.js
@@ -6,17 +6,20 @@ class CommandCd extends CommandBase {
     constructor() {
         super('cd');
         this._pathHandler = getPathHandler();
-        this._usage = `cd [PATH?]`;
-        this._description = `Changes CWD to PATH.${EOL}If [PATH] is not provided, CWD would be changed to HOMEDIR`;
+        this._usage = `cd [PATH]`;
+        this._description = `Changes CWD to PATH`;//${EOL}If [PATH] is not provided, CWD would be changed to HOMEDIR`;
     }
 
-    validateParameters = (args) => true;
+    validateParameters(args) {
+        // Lead to Invalid input error if cd has no parameters
+        return args.length > 0;
+    };
 
     async performCommand(args) {
-        if (args.length === 0) {
-            await this._pathHandler.cd(this._pathHandler.homePath);
-            return;
-        }
+        // if (args.length === 0) {
+        //     await this._pathHandler.cd(this._pathHandler.homePath);
+        //     return;
+        // }
         const path = args[0];
         await this._pathHandler.cd(path);
     }
