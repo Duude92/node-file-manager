4b420cae3038aae9c83b9cb7bd2ad1fb0b231d3c
Duude92 <switch4acc@gmail.com>
2025-05-02 20:28:13 +0300

    Implementation of parameters validation, usage and description for command mkdir

--- src/commands/fs/commands/commandMkdir.js
+++ src/commands/fs/commands/commandMkdir.js
@@ -1,16 +1,24 @@
-import { CommandBase } from "../../commandBase.js";
+import {CommandBase} from "../../commandBase.js";
 import fs from 'node:fs/promises';
 
 class CommandMkdir extends CommandBase {
     constructor() {
         super("mkdir");
+        this._usage = `mkdir [PATH]`;
+        this._description = `Create new directory in current working directory.`;
+    }
+
+    validateParameters(args) {
+        return args.length > 0 && this._pathHandler.validateDirectoryName(args[0]);
     }
 
     async performCommand(args) {
+
         const dirPath = this._pathHandler.resolvePath(args[0]);
         // TODO: Ask rsschool if it is necessary to create directory recursively
-        await fs.mkdir(dirPath, { recursive: false })
+        await fs.mkdir(dirPath, {recursive: false})
             .then(() => console.log(`Directory created: ${dirPath}`));
     }
 }
+
 export const createCommand = () => new CommandMkdir();
