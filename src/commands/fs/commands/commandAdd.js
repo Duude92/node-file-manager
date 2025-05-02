52bd3af5a4a22a74ec250a62ec7d1d40df934ff4
Duude92 <switch4acc@gmail.com>
2025-05-02 15:32:52 +0300

    Implementation of parameters validation, usage and description for command add

--- src/commands/fs/commands/commandAdd.js
+++ src/commands/fs/commands/commandAdd.js
@@ -1,16 +1,24 @@
-import { CommandBase } from "../../commandBase.js";
+import {CommandBase} from "../../commandBase.js";
 import fs from 'node:fs/promises';
 import {displayResultLine} from "../../../messages/messageManager.mjs";
+import {EOL} from "node:os";
 
 class CommandAdd extends CommandBase {
     constructor() {
         super("add");
+        this._usage = `add [FILE]`;
+        this._description = 'Creates empty file with name [FILE]';
+    }
+
+    validateParameters(args) {
+        return args.length > 0;
     }
 
     async performCommand(args) {
         const filePath = this._pathHandler.resolvePath(args[0]);
-        await fs.writeFile(filePath, '', { flag: 'wx' });
+        await fs.writeFile(filePath, '', {flag: 'wx'});
         displayResultLine(`File ${filePath} successfully created`);
     }
 }
+
 export const createCommand = () => new CommandAdd();
